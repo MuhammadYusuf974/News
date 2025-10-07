933986a9619bf1109aefd536946debe6dc71e9dc
Yusuf20110529 <thedafanmade@gmail.com>
2025-10-07 20:47:05 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const signInBtn = document.getElementById("signInBtn");
+const registerContainer = document.getElementById("registerContainer");
+
+// Modalni ochish
+signInBtn.addEventListener("click", () => {
+  const signInModal = new bootstrap.Modal(document.getElementById("signInModal"));
+  signInModal.show();
+});
+
+// Shunchaki demo uchun status
+document.getElementById("sendCode").onclick = () => {
+  document.getElementById("status").innerText = "Kod yuborildi! (demo)";
+  document.getElementById("step1").style.display = "none";
+  document.getElementById("step2").style.display = "block";
+};
+
+document.getElementById("verifyCode").onclick = () => {
+  document.getElementById("status").innerText = "Kod tasdiqlandi!";
+  document.getElementById("step2").style.display = "none";
+  document.getElementById("step3").style.display = "block";
+};
+
+document.getElementById("createAccount").onclick = () => {
+  document.getElementById("status").innerText = "Hisob yaratildi! Endi Sign In orqali kiring.";
+};
